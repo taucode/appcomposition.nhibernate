18177e5c567c0d86e0380294234b32de8c6db54c
Andrey Kovalenko <ak@taucode.com>
2023-02-06 19:46:18 +0200

    Winter resurrection - 2023.

--- pre-deploy.cmd
+++ pre-deploy.cmd
@@ -1,12 +1,9 @@
 dotnet restore
 
-dotnet clean --configuration Debug
-dotnet clean --configuration Release
+dotnet build TauCode.AppComposition.NHibernate.sln -c Debug
+dotnet build TauCode.AppComposition.NHibernate.sln -c Release
 
-dotnet build --configuration Debug
-dotnet build --configuration Release
-
-dotnet test -c Debug .\test\TauCode.AppComposition.NHibernate.Tests\TauCode.AppComposition.NHibernate.Tests.csproj
-dotnet test -c Release .\test\TauCode.AppComposition.NHibernate.Tests\TauCode.AppComposition.NHibernate.Tests.csproj
+dotnet test TauCode.AppComposition.NHibernate.sln -c Debug
+dotnet test TauCode.AppComposition.NHibernate.sln -c Release
 
 nuget pack nuget\TauCode.AppComposition.NHibernate.nuspec
